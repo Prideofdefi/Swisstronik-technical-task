b257b830ed3c3a9e897e0045b6a87dccb4ba57af
Prideofdefi <wisdomsundayjamb@gmail.com>
2024-08-30 12:25:19 +0100

    Create Wisdom.sh

---
+++ Wisdom.sh
@@ -0,0 +1,7 @@
+echo "# Swisstronik-technical-task" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Prideofdefi/Swisstronik-technical-task.git
+git push -u origin main
